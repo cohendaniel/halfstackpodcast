7eb5eb26ef04b34afa07257fe2e9f6de563849a1
cchanEG <cchan@evergage.com>
2019-02-26 21:23:26 -0500

    First draft of the ABOUT page, including a SLICK SLIDER

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Slider from 'react-slick'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
@@ -6,6 +7,13 @@ import Content, { HTMLContent } from '../components/Content'
 
 export const AboutPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content
+  const settings = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000
+  }
 
   return (
     <section className="section section--gradient">
@@ -13,6 +21,28 @@ export const AboutPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
+            <div className="carousel-container" style={{maxWidth: '400px', margin: '10px auto 50px auto', display: 'block', position: 'relative'}}>
+                <Slider {...settings}>
+                  <div className="bio-slide">
+                    <figure>
+                      <img src="https://media.licdn.com/dms/image/C4E03AQFiqT-huc-4Tw/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=-p4sRCF5wAt1EChcbuiFoWiLaXduS4Ik2o1oVBfbpbs"/>
+                      <figcaption style={{textAlign: 'center', fontWeight: 'bold', fontSize: '18px'}}>Dan</figcaption>
+                    </figure>
+                  </div>
+                  <div className="bio-slide">
+                    <figure>
+                      <img src="https://media.licdn.com/dms/image/C4E03AQFVIh10wuudcQ/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=cigyct1v7j8E2Vr-Og09CwK7QhJCtqeT-ORcq3AFRz0"/>
+                      <figcaption style={{textAlign: 'center', fontWeight: 'bold', fontSize: '18px'}}>David (Destrich)</figcaption>
+                    </figure>
+                  </div>
+                  <div className="bio-slide">
+                    <figure>
+                      <img src="https://media.licdn.com/dms/image/C4E03AQGV3jwhpnbjqw/profile-displayphoto-shrink_800_800/0?e=1556755200&v=beta&t=7UsCaax-MiIfIA6ZTUc43NzfPLBCHc-iTS7_IeOWsU8"/>
+                      <figcaption style={{textAlign: 'center', fontWeight: 'bold', fontSize: '18px'}}>Connor</figcaption>
+                    </figure>
+                  </div>
+                </Slider>
+              </div>
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
